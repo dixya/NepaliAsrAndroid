2e8d99751372998d4d3f259f438a4ac4174842c6
dixya <lamichhanedixya@gmail.com>
2017-08-10 18:06:03 -0700

    Call to  aws serveradded in the Main_Activity page

--- androidapp/src/main/java/org/nepalitools/asr/androidapp/MainActivity.java
+++ androidapp/src/main/java/org/nepalitools/asr/androidapp/MainActivity.java
@@ -1,5 +1,6 @@
 package org.nepalitools.asr.androidapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
@@ -7,6 +8,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -14,7 +16,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 import static android.widget.Toast.*;
@@ -39,6 +47,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button stopPlayBtn;
 
     private TextView text;
+    private TextView messageText;
+    int serverResponseCode = 0;
+    ProgressDialog dialog = null;
+    /**********  File Path *************/
+    final String uploadFilePath = "/mnt/sdcard/";
+    final String uploadFileName = outputFile;
+    String upLoadServerUri = "http://nepaliasr.us-west-2.elasticbeanstalk.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -315,20 +330,186 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         if (view == submitButton) {
 
-//public void sendMessage(View view){
-                Intent intent = new Intent(this, UploadToServer.class);
-                Bundle b=new Bundle();
-                b.putString("uploadFileName",outputFile);
-                Editable editText = textContent.getText();
-                String message = editText.toString();
-                b.putString("textValue",message);
+            submitButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-                intent.putExtras(b);
-                startActivity(intent);
+                   // dialog = ProgressDialog.show(this, "", "Uploading file...", true);
 
-           // }
+                    new Thread(new Runnable() {
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    messageText.setText("uploading started.....");
+                                }
+                            });
+
+                            uploadFile(uploadFilePath + "" + uploadFileName);
+
+                        }
+                    }).start();
+                }
+            });
         }
     }
+
+    public int uploadFile(String sourceFileUri) {
+
+
+        String fileName = sourceFileUri;
+
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+
+        if (!sourceFile.isFile()) {
+
+            dialog.dismiss();
+
+            Log.e("uploadFile", "Source File not exist :"
+                    +uploadFilePath + "" + uploadFileName);
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    messageText.setText("Source File not exist :"
+                            +uploadFilePath + "" + uploadFileName);
+                }
+            });
+
+            return 0;
+
+        }
+        else
+        {
+            try {
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(upLoadServerUri);
+
+                // Open a HTTP  connection to  the URL
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", fileName);
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; filename="
+                        + fileName + "" + lineEnd);
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                if(serverResponseCode == 200){
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+
+                            String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
+                                    +" http://www.androidexample.com/media/uploads/"
+                                    +uploadFileName;
+
+                            messageText.setText(msg);
+                           // Toast.makeText(UploadToServer.this, "File Upload Complete.",
+                                  //  Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+
+                dialog.dismiss();
+                ex.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        messageText.setText("MalformedURLException Exception : check script url.");
+                       // Toast.makeText(UploadToServer.this, "MalformedURLException",
+                             //   Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+            } catch (Exception e) {
+
+                dialog.dismiss();
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        messageText.setText("Got Exception : see logcat ");
+                       // Toast.makeText(UploadToServer.this, "Got Exception : see logcat ",
+                               // Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Log.e("Upload file to server Exception", "Exception : "
+                        + e.getMessage(), e);
+            }
+            dialog.dismiss();
+            return serverResponseCode;
+
+        } // End else block
+    }
+
+//public void sendMessage(View view){
+//                Intent intent = new Intent(this, UploadToServer.class);
+//                Bundle b=new Bundle();
+//                b.putString("uploadFileName",outputFile);
+//                Editable editText = textContent.getText();
+//                String message = editText.toString();
+//                b.putString("textValue",message);
+//
+//                intent.putExtras(b);
+//                startActivity(intent);
+
+           // }
+
+
 
     private void makeServerCall() {
         String urlString = "http://nepaliasr.us-west-2.elasticbeanstalk.com/ping";
